60bc47929d27fec3121b52cd504140b29addf402
Eric Dahlgren <eric@nodeiwest.se>
2021-12-29 20:36:36 +0100

    yarn

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,1 +1,7 @@
-const supabase = createClient
+import {createClient} from '@supabase/supabase-js';
+
+const url = 'https://fwrvehmsgivqnzyggwbt.supabase.co';
+const key =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyNTQyNjYzMSwiZXhwIjoxOTQxMDAyNjMxfQ.i3szYZ0r92Yb91WJQ-BCpHLR7ZDCsXxTYK_V8VJ4do8';
+
+export const supabase = createClient(url, key);
