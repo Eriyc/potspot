20d9649a9ab5321e67370f3f116e05377fc28936
Eric Dahlgren <eric@nodeiwest.se>
2021-12-29 22:49:02 +0100

    use react hook form

--- src/features/account/screens/SignInScreen.tsx
+++ src/features/account/screens/SignInScreen.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import {Pressable, ScrollView, Text, TextInput, View} from 'react-native';
 import tw from 'twrnc';
 
@@ -6,9 +6,35 @@ import {useNavigation} from '@react-navigation/native';
 
 import {Background} from '../components';
 import {AuthRoute} from '../navigation';
+import {Controller, useForm} from 'react-hook-form';
+
+interface FormData {
+  email: string;
+  password: string;
+}
+
+const formStyle = tw`text-black bg-gray-100 rounded-md mb-2`;
 
 export const SignInScreen = () => {
   const navigation = useNavigation<AuthRoute>();
+
+  const {
+    control,
+    handleSubmit,
+    watch,
+    reset,
+    formState: {errors},
+  } = useForm<FormData>();
+
+  const passwordRef = useRef<string>('');
+  passwordRef.current = watch('password', '');
+
+  const onSubmit = (data: any) => {
+    console.log('data', data);
+  };
+  const onError = () => {
+    console.log('error', errors);
+  };
 
   return (
     <Background>
@@ -23,9 +49,51 @@ export const SignInScreen = () => {
         <View style={tw`flex flex-1 justify-center`}>
           <View style={tw`bg-white m-4 rounded-xl p-8`}>
             <Text style={tw`text-black`}>Email</Text>
-            <TextInput style={tw`text-black bg-gray-100 rounded-md mb-2`} />
+            <Controller
+              control={control}
+              rules={{
+                required: true,
+              }}
+              render={({field: {onChange, onBlur, value}}) => (
+                <TextInput
+                  style={formStyle}
+                  onBlur={onBlur}
+                  onChangeText={onChange}
+                  value={value}
+                  keyboardType="email-address"
+                  textContentType="emailAddress"
+                />
+              )}
+              name="email"
+            />
+            <Text style={tw`text-red-500 mb-2`}>
+              {errors.email?.type === 'pattern' && 'Ange en giltig mailaddress'}
+            </Text>
             <Text style={tw`text-black`}>Lösenord</Text>
-            <TextInput style={tw`text-black bg-gray-100 rounded-md mb-4`} />
+
+            <Controller
+              control={control}
+              rules={{
+                minLength: 6,
+              }}
+              render={({field: {onChange, onBlur, value}}) => (
+                <TextInput
+                  style={formStyle}
+                  onBlur={onBlur}
+                  onChangeText={onChange}
+                  value={value}
+                  placeholder="Lösenord"
+                  secureTextEntry
+                  textContentType="password"
+                  onSubmitEditing={handleSubmit(onSubmit, onError)}
+                />
+              )}
+              name="password"
+            />
+            <Text style={tw`text-red-500 mb-2`}>
+              {errors.password?.type === 'minLength' &&
+                'Lösenordet måste vara längre än 6 karaktärer'}
+            </Text>
             <View style={tw`flex flex-row items-center`}>
               <Pressable
                 style={({pressed}) =>
@@ -34,7 +102,9 @@ export const SignInScreen = () => {
                     pressed && 'bg-indigo-300',
                   )
                 }
-                onPress={() => {}}>
+                onPress={() => {
+                  handleSubmit(onSubmit, onError);
+                }}>
                 <Text style={tw`text-white`}>Logga in</Text>
               </Pressable>
               <Text style={tw`text-black text-center flex-1 ml-2`}>
--- src/features/account/screens/SignUpScreen.tsx
+++ src/features/account/screens/SignUpScreen.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import {Pressable, ScrollView, Text, TextInput, View} from 'react-native';
+import {useForm, Controller} from 'react-hook-form';
+
 import tw from 'twrnc';
 
 import {useNavigation} from '@react-navigation/native';
@@ -7,11 +9,32 @@ import {useNavigation} from '@react-navigation/native';
 import {AuthRoute} from '../navigation';
 import {Background} from '../components';
 
+interface FormData {
+  email: string;
+  password: string;
+  confirm: string;
+}
+
+const formStyle = tw`text-black bg-gray-100 rounded-md mb-2`;
+
 export const SignupScreen = () => {
   const navigation = useNavigation<AuthRoute>();
+  const {
+    control,
+    handleSubmit,
+    watch,
+    reset,
+    formState: {errors},
+  } = useForm<FormData>();
 
-  const handleSignUp = () => {
-    
+  const passwordRef = useRef<string>('');
+  passwordRef.current = watch('password', '');
+
+  const onSubmit = (data: any) => {
+    console.log('data', data);
+  };
+  const onError = () => {
+    console.log('error', errors);
   };
 
   return (
@@ -27,11 +50,77 @@ export const SignupScreen = () => {
         <View style={tw`flex flex-1 justify-center`}>
           <View style={tw`bg-white m-4 rounded-xl p-8`}>
             <Text style={tw`text-black`}>Email</Text>
-            <TextInput style={tw`text-black bg-gray-100 rounded-md mb-2`} />
+            <Controller
+              control={control}
+              rules={{
+                required: true,
+              }}
+              render={({field: {onChange, onBlur, value}}) => (
+                <TextInput
+                  style={formStyle}
+                  onBlur={onBlur}
+                  onChangeText={onChange}
+                  value={value}
+                  keyboardType="email-address"
+                  textContentType="emailAddress"
+                />
+              )}
+              name="email"
+            />
+            <Text style={tw`text-red-500 mb-2`}>
+              {errors.email?.type === 'pattern' && 'Ange en giltig mailaddress'}
+            </Text>
+
             <Text style={tw`text-black`}>Lösenord</Text>
-            <TextInput style={tw`text-black bg-gray-100 rounded-md mb-4`} />
-            <Text style={tw`text-black`}>Bekräfta lösenord</Text>
-            <TextInput style={tw`text-black bg-gray-100 rounded-md mb-4`} />
+
+            <Controller
+              control={control}
+              rules={{
+                minLength: 6,
+              }}
+              render={({field: {onChange, onBlur, value}}) => (
+                <TextInput
+                  style={formStyle}
+                  onBlur={onBlur}
+                  onChangeText={onChange}
+                  value={value}
+                  placeholder="Lösenord"
+                  secureTextEntry
+                  textContentType="newPassword"
+                />
+              )}
+              name="password"
+            />
+            <Text style={tw`text-red-500 mb-2`}>
+              {errors.password?.type === 'minLength' &&
+                'Lösenordet måste vara längre än 6 karaktärer'}
+            </Text>
+
+            <Text style={tw`text-black`}>Bekräfta lösenordet</Text>
+
+            <Controller
+              control={control}
+              rules={{
+                maxLength: 100,
+                validate: pw =>
+                  pw === passwordRef.current ||
+                  'Lösenorden stämmer inte överrens',
+              }}
+              render={({field: {onChange, onBlur, value}}) => (
+                <TextInput
+                  style={tw`text-black bg-gray-100 rounded-md mb-2`}
+                  onBlur={onBlur}
+                  onChangeText={onChange}
+                  value={value}
+                  secureTextEntry
+                  textContentType="newPassword"
+                  onSubmitEditing={handleSubmit(onSubmit, onError)}
+                />
+              )}
+              name="confirm"
+            />
+            <Text style={tw`text-red-500 mb-2`}>{errors.confirm?.message}</Text>
+
             <Pressable
               style={({pressed}) =>
                 tw.style(
@@ -39,14 +128,21 @@ export const SignupScreen = () => {
                   pressed && 'bg-indigo-300',
                 )
               }
-              onPress={() => {}}>
+              onPress={handleSubmit(onSubmit, onError)}>
               <Text style={tw`text-white`}>Skapa konto</Text>
             </Pressable>
           </View>
         </View>
         <View style={tw`py-8`}>
           <Text
-            onPress={() => navigation.navigate('signin')}
+            onPress={() => {
+              reset({
+                confirm: '',
+                email: '',
+                password: '',
+              });
+              navigation.navigate('signin');
+            }}
             style={tw`text-gray-200 text-lg text-center`}>
             Jag har redan ett konto
           </Text>
