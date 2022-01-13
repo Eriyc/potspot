b547b85179cc224cf71da5d3775ce28026d86770
Eriyc <eric@nodeiwest.se>
2022-01-13 19:07:36 +0100

    add mapbox

--- src/Entrypoint.tsx
+++ src/Entrypoint.tsx
@@ -4,6 +4,11 @@ import React from 'react';
 import tw, {useAppColorScheme, useDeviceContext} from 'twrnc';
 import {MainNavigation} from './Navigation';
 import {StoreProvider} from './store';
+import MapboxGL from '@react-native-mapbox-gl/maps';
+
+MapboxGL.setAccessToken(
+  'pk.eyJ1IjoiZXJpeWMiLCJhIjoiY2ttNTUwenZiMGIyMDJvbXR6OTZuazdwNyJ9._NfBwvdZ_yuB-9xYX7phbg',
+);
 
 const Entrypoint = () => {
   // 1️⃣  opt OUT of listening to DEVICE color scheme events
--- src/features/trap/screens/map.tsx
+++ src/features/trap/screens/map.tsx
@@ -1,12 +1,32 @@
-import React, {FC} from 'react';
+import React, {FC, useRef} from 'react';
 import {observer} from 'mobx-react-lite';
-import {View, Text} from 'react-native';
+import {View} from 'react-native';
 import tw from 'twrnc';
+import MapboxGL from '@react-native-mapbox-gl/maps';
+
+const INITIAL_POS: [number, number] = [11.605112551760044, 57.637770511076575];
+
+const openSeaMapTiles = {
+  tileUrlTemplates: ['https://tiles.openseamap.org/seamark/{z}/{x}/{y}.png'],
+  tileSize: 256,
+  id: 'openseamap',
+};
 
 export const MapScreen: FC = observer(() => {
+  const cameraRef = useRef<MapboxGL.Camera>(null);
+
   return (
-    <View style={tw`flex flex-1 justify-center items-center bg-gray-700`}>
-      <Text>Map screen</Text>
+    <View style={tw`flex-1 bg-gray-700`}>
+      <MapboxGL.MapView style={tw`flex-1`}>
+        <MapboxGL.UserLocation />
+        <MapboxGL.Camera
+          ref={cameraRef}
+          defaultSettings={{centerCoordinate: INITIAL_POS}}
+        />
+        <MapboxGL.RasterSource {...openSeaMapTiles}>
+          <MapboxGL.RasterLayer id="openseamapLayer" sourceID="openseamap" />
+        </MapboxGL.RasterSource>
+      </MapboxGL.MapView>
     </View>
   );
 });
