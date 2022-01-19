09028f7202165a941b844080cf3a962c31ada183
Eric Dahlgren <eric@dgren.dev>
2022-01-19 08:46:37 +0100

    feat: add trap screen and functionality

---
+++ src/store/TrapStore.ts
@@ -0,0 +1,48 @@
+import {PostgrestError} from '@supabase/supabase-js';
+import {Point} from 'geojson';
+import {flow, SnapshotOut} from 'mobx-state-tree';
+import {types} from 'mobx-state-tree';
+
+import {supabase} from '@/utils/supabase';
+
+interface NewTrap {
+  displayname: string;
+  pos: Point;
+}
+
+type SupabaseResponse<T> = {
+  body: null | T;
+  error: null | PostgrestError;
+};
+
+export const Trap = types.model('Trap', {
+  id: types.identifierNumber,
+  createdBy: types.string,
+  pos: types.string,
+  updated_at: types.string,
+  displayname: types.optional(types.string, 'Tina utan namn'),
+  in_use: types.boolean,
+});
+
+export const TrapStore = types
+  .model('TrapStore', {
+    traps: types.array(Trap),
+    selected: types.maybeNull(types.reference(Trap)),
+  })
+  .actions(self => ({
+    new: flow(function* (trap: NewTrap) {
+      const {body, error}: SupabaseResponse<TrapSnapshot> = yield supabase.rpc(
+        'create_trap_rpc',
+        {
+          details: {
+            displayname: trap.displayname,
+            pos: JSON.stringify(trap.pos),
+          },
+        },
+      );
+
+      self.traps.push(Trap);
+    }),
+  }));
+
+type TrapSnapshot = SnapshotOut<typeof Trap>;
