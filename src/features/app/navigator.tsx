d8a2f4324a6ec065d91ea4f17a25786616553472
eric <50216491+Eriyc@users.noreply.github.com>
2022-03-20 10:29:17 +0100

    fix dark mode and some colors

--- src/features/app/navigator.tsx
+++ src/features/app/navigator.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import {Inbox, Map, Settings} from 'react-native-feather';
 import {useTailwind} from 'tailwind-rn/dist';
 
+import {Text} from '@/components/Text';
+
 import {SettingsScreen} from './screens';
 import {FeedNavigation} from '../feed';
 import {TrapNavigator} from '../trap/navigator';
@@ -21,12 +23,17 @@ export const AppNavigation = () => {
   return (
     <>
       <AppStack.Navigator
-        sceneContainerStyle={tw('dark:bg-gray-900')}
+        sceneContainerStyle={tw('dark:bg-zinc-900')}
         screenOptions={{
           tabBarShowLabel: false,
           tabBarStyle: tw('bg-gray-50 dark:bg-gray-700'),
-          tabBarActiveTintColor: tw('primary-light dark:primary-dark')
+          tabBarActiveTintColor: tw('text-violet-600 dark:text-violet-400')
             .color as string,
+
+          headerStyle: tw('bg-gray-50 dark:bg-gray-700'),
+          headerTitle: ({children}) => (
+            <Text style={tw('text-lg font-bold')}>{children}</Text>
+          ),
         }}>
         <AppStack.Screen
           component={TrapNavigator}
@@ -49,8 +56,6 @@ export const AppNavigation = () => {
         <AppStack.Screen
           component={SettingsScreen}
           options={{
-            headerTitleStyle: tw('text-black dark:text-white'),
-            headerStyle: tw('bg-white dark:bg-zinc-700 shadow-md'),
             tabBarIcon: ({color}) => <Settings color={color} />,
           }}
           name="settings"
--- src/features/trap/screens/map.tsx
+++ src/features/trap/screens/map.tsx
@@ -1,6 +1,7 @@
 import MapboxGL from '@react-native-mapbox-gl/maps';
 import React, {FC, useCallback, useRef} from 'react';
 import {Dimensions, FlatList, View, ViewToken} from 'react-native';
+import ErrorBoundary from 'react-native-error-boundary';
 import {useTailwind} from 'tailwind-rn/dist';
 
 import {useLocationPermission} from '@/hooks/useLocationPermission';
@@ -10,6 +11,10 @@ import {useColorScheme} from '@/utils/colorScheme';
 import {INITIAL_POS} from '..';
 import {CreateNewTrapButton, TrapMapCard} from '../components';
 import {Trap, useTraps} from '../hooks/useTraps';
+
+const handleError = (error: Error) => {
+  console.log(error);
+};
 
 const openSeaMapTiles = {
   tileUrlTemplates: ['https://tiles.openseamap.org/seamark/{z}/{x}/{y}.png'],
@@ -40,52 +45,54 @@ export const MapScreen: FC = () => {
 
   return (
     <View style={tw(`flex-1 bg-gray-700`)}>
-      <MapboxGL.MapView
-        style={tw(`flex-1`)}
-        styleURL={
-          dark
-            ? 'mapbox://styles/mapbox/dark-v9'
-            : 'mapbox://styles/mapbox/satellite-v9'
-        }>
-        {locationGranted && <MapboxGL.UserLocation />}
-        <MapboxGL.Camera
-          ref={cameraRef}
-          defaultSettings={{centerCoordinate: INITIAL_POS, zoomLevel: 8}}
-          maxZoomLevel={13}
+      <ErrorBoundary onError={handleError}>
+        <MapboxGL.MapView
+          style={tw(`flex-1`)}
+          styleURL={
+            dark
+              ? 'mapbox://styles/mapbox/dark-v9'
+              : 'mapbox://styles/mapbox/satellite-v9'
+          }>
+          {locationGranted && <MapboxGL.UserLocation />}
+          <MapboxGL.Camera
+            ref={cameraRef}
+            defaultSettings={{centerCoordinate: INITIAL_POS, zoomLevel: 8}}
+            maxZoomLevel={13}
+          />
+          <MapboxGL.RasterSource {...openSeaMapTiles}>
+            <MapboxGL.RasterLayer id="openseamapLayer" sourceID="openseamap" />
+          </MapboxGL.RasterSource>
+          {traps &&
+            traps.map(trap => (
+              <MapboxGL.PointAnnotation
+                coordinate={trap.pos as unknown as [number, number]}
+                id={`marker-${trap.id}-${dark.valueOf()}`}
+                key={`marker-${trap.id}-${dark.valueOf()}`}>
+                <View
+                  style={tw(
+                    `bg-pink-500 border-2 border-white h-4 rounded-full w-4`,
+                  )}
+                />
+              </MapboxGL.PointAnnotation>
+            ))}
+        </MapboxGL.MapView>
+        <FlatList
+          style={tw(`absolute bottom-0`)}
+          contentContainerStyle={{
+            padding: Dimensions.get('screen').width * 0.1,
+          }}
+          snapToAlignment="center"
+          horizontal
+          snapToInterval={Dimensions.get('screen').width * 0.85}
+          decelerationRate="fast"
+          ListFooterComponent={<CreateNewTrapButton />}
+          data={traps}
+          keyExtractor={({id}) => id.toString()}
+          renderItem={({item}) => <TrapMapCard item={item} />}
+          onViewableItemsChanged={handleScrollFinish}
+          viewabilityConfig={{itemVisiblePercentThreshold: 50}}
         />
-        <MapboxGL.RasterSource {...openSeaMapTiles}>
-          <MapboxGL.RasterLayer id="openseamapLayer" sourceID="openseamap" />
-        </MapboxGL.RasterSource>
-        {traps &&
-          traps.map(trap => (
-            <MapboxGL.PointAnnotation
-              coordinate={trap.pos as unknown as [number, number]}
-              id={`marker-${trap.id}-${dark.valueOf()}`}
-              key={`marker-${trap.id}-${dark.valueOf()}`}>
-              <View
-                style={tw(
-                  `bg-pink-500 border-2 border-white h-4 rounded-full w-4`,
-                )}
-              />
-            </MapboxGL.PointAnnotation>
-          ))}
-      </MapboxGL.MapView>
-      <FlatList
-        style={tw(`absolute bottom-0`)}
-        contentContainerStyle={{
-          paddingLeft: Dimensions.get('screen').width * 0.1,
-        }}
-        snapToAlignment="center"
-        horizontal
-        snapToInterval={Dimensions.get('screen').width * 0.85}
-        decelerationRate="fast"
-        ListFooterComponent={<CreateNewTrapButton />}
-        data={traps}
-        keyExtractor={({id}) => id.toString()}
-        renderItem={({item}) => <TrapMapCard item={item} />}
-        onViewableItemsChanged={handleScrollFinish}
-        viewabilityConfig={{itemVisiblePercentThreshold: 50}}
-      />
+      </ErrorBoundary>
     </View>
   );
 };
