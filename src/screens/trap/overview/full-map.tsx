b6d95997dfc3a17250082855c85857ff9fb29da9
Eric Dahlgren <eric@dgren.dev>
2022-05-27 14:41:00 +0200

    more trap screens

--- src/screens/trap/overview/full-map.tsx
+++ src/screens/trap/overview/full-map.tsx
@@ -1,16 +1,12 @@
-import React, {useRef} from 'react';
+import React, {useCallback, useEffect, useMemo, useRef} from 'react';
 import Mapbox, {OnPressEvent, SymbolLayerStyle} from '@rnmapbox/maps';
-import {View} from 'ui';
-import {TrapFeatureCollection} from 'api/trap';
+import {MapBase, Pressable, Text, View, WIDTH} from 'ui';
+import {TrapFeatureCollection, TrapFeatureType} from 'api/trap';
 import {useMapState} from './map-state';
+import {useNavigation} from '@react-navigation/native';
+import {TrapNavigationProp} from 'navigation/trap-navigator';
 
 const mapstyle = {flex: 1};
-
-const openSeaMapTiles = {
-  tileUrlTemplates: ['https://tiles.openseamap.org/seamark/{z}/{x}/{y}.png'],
-  tileSize: 256,
-  id: 'openseamap',
-};
 
 export const INITIAL_POS: [number, number] = [
   11.605112551760044, 57.637770511076575,
@@ -19,34 +15,66 @@ type TrapMapProps = {
   traps?: TrapFeatureCollection;
 };
 
+const SelectedPin = ({trap}: {trap: TrapFeatureType}) => {
+  const navigation = useNavigation<TrapNavigationProp<'overview'>>();
+
+  return (
+    <Mapbox.MarkerView
+      coordinate={trap.geometry.coordinates}
+      id={`trap-${trap.id}`}>
+      <View bg="white" width={WIDTH * 0.5}>
+        <Pressable
+          p="m"
+          onPress={() =>
+            navigation.navigate('details', {id: parseInt(trap.id!.toString())})
+          }>
+          <Text>{trap.properties.displayname}</Text>
+        </Pressable>
+      </View>
+    </Mapbox.MarkerView>
+  );
+};
+
 export const FullTrapMap = ({traps}: TrapMapProps) => {
   const mapState = useMapState();
-  const camera = useRef;
+  const camera = useRef<Mapbox.Camera>(null);
+
+  useEffect(() => {
+    camera.current?.setCamera({
+      centerCoordinate: traps?.features.find(i => i.id == mapState.selectedTrap)
+        ?.geometry.coordinates,
+      animationDuration: 200,
+      zoomLevel: 10,
+    });
+  }, [mapState.selectedTrap]);
 
   const handlePinPress = ({features}: OnPressEvent) => {
     const trap = features[0];
     const id = trap.id;
 
-    if (!id || typeof id !== 'number') return;
-    mapState.setSelectedTrap(id);
+    if (!id) return;
+    mapState.setSelectedTrap(parseInt(id.toString()));
   };
+
+  const selectedTrap = useMemo(
+    () => traps?.features?.find(t => t.id === mapState.selectedTrap)!,
+    [mapState.selectedTrap],
+  );
+
+  const closeSelected = useCallback(() => {
+    if (mapState.selectedTrap) {
+      mapState.setSelectedTrap(null);
+    }
+  }, [mapState.selectedTrap]);
 
   return (
     <View flex={1}>
-      <Mapbox.MapView
-        styleURL="mapbox://styles/mapbox/satellite-v9"
-        style={mapstyle}
-        onDidFinishRenderingMap={() => console.log('rendered')}
-        onDidFinishLoadingMap={() => console.log('loaded')}
-        onWillStartLoadingMap={() => console.log('start loading')}
-        onWillStartRenderingMap={() => console.log('start render')}>
+      <MapBase onPress={closeSelected} style={mapstyle}>
         <Mapbox.Camera
+          ref={camera}
           defaultSettings={{centerCoordinate: INITIAL_POS, zoomLevel: 8}}
         />
-        <Mapbox.UserLocation />
-        <Mapbox.RasterSource {...openSeaMapTiles}>
-          <Mapbox.RasterLayer id="openseamapLayer" sourceID="openseamap" />
-        </Mapbox.RasterSource>
+
         {traps && (
           <Mapbox.ShapeSource
             id="trap-locations"
@@ -55,7 +83,8 @@ export const FullTrapMap = ({traps}: TrapMapProps) => {
             <Mapbox.SymbolLayer id="mapPinsLayer" style={markerStyle} />
           </Mapbox.ShapeSource>
         )}
-      </Mapbox.MapView>
+        {mapState.selectedTrap && <SelectedPin trap={selectedTrap} />}
+      </MapBase>
     </View>
   );
 };
